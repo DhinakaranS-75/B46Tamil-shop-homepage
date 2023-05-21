cef28a7338974b865a53039b785534780d002c4c
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-05-21 22:35:50 +0530

    new code

--- src/Sales/Sales.js
+++ src/Sales/Sales.js
@@ -4,12 +4,146 @@ import './Sales.css'
 function Sales() {
   return (
     <div>
-          <section >
-            <div class="container ">
-                <div class="row ">                  
-                </div>
-            </div>
-        </section>
+      <div className='waraper' id='1'>
+        
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <hr />
+
+        <div className='card-body'>
+          <div className='card-body-item'>
+            <h3>Fancy Product</h3>
+            <p>$50.00-$25.00</p>
+            <span></span>
+          </div>
+          <div class="card-footer">
+              <a href="#">View Options</a>
+          </div>    
+        </div>
+
+      </div>
+      <div className='waraper' id='2'>
+        <div className='sales-icon'>Sale</div>
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <hr />
+
+        <div className='card-body'>
+          <div className='card-body-item'>
+            <h3>Special Item</h3>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <p>$20.00 $18.00</p>
+            <span></span>
+          </div>
+          <a href="#">Add to cart</a>
+            
+        </div>
+
+      </div>
+      <div className='waraper ' id='3'>
+        <div className='sales-icon'>Sale</div>
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <hr />
+
+        <div className='card-body'>
+          <div className='card-body-item'>
+            <h3>Sale Item</h3>
+            <p>$50.00 $25.00</p>
+            <span></span>
+          </div>
+          <div class="card-footer">
+              <a href="#">Add to cart</a>
+          </div>    
+        </div>
+
+      </div><div className='waraper' id='4'>
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <hr />
+
+        <div className='card-body'>
+          <div className='card-body-item'>
+            <h3>Popular Item</h3>
+            <p>$40.00</p>
+            <span></span>
+          </div>
+          <div class="card-footer">
+              <a href="#">Add to cart</a>
+          </div>    
+        </div>
+
+      </div>
+      
+      <div className='waraper' id='5'>
+        <div className='sales-icon'>Sale</div>
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <hr />
+
+        <div className='card-body'>
+          <div className='card-body-item'>
+            <h3>Sale Item</h3>
+            <p>$50.00 $25.00</p>
+            <span></span>
+          </div>
+          <div class="card-footer">
+            <a href="#">Add to cart</a>
+          </div>    
+        </div>
+
+      </div>
+
+      <div className='waraper' id='6'>
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <hr />
+
+        <div className='card-body'>
+          <div className='card-body-item'>
+            <h3>Fancy Product</h3>
+            <p>$120.00 - $280.00</p>
+            <span></span>
+          </div>
+          <div class="card-footer">
+              <a href="#">View Options</a>
+          </div>    
+        </div>
+
+      </div>
+
+      <div className='waraper' id='7'>
+        <div className='sales-icon'>Sale</div>
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <hr />
+
+        <div className='card-body'>
+          <div className='card-body-item'>
+            <h3>Special Item</h3>
+            <p>$20.00 $18.00</p>
+            <span></span>
+          </div>
+          <div class="card-footer">
+              <a href="#">Add to cart</a>
+          </div>    
+        </div>
+
+      </div>
+      <div className='waraper' id='8'>
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <hr />
+
+        <div className='card-body'>
+          <div className='card-body-item'>
+            <h3>Popular Item</h3>
+            <p>$40.00</p>
+            <span></span>
+          </div>
+          <div class="card-footer">
+              <a href="#">Add to cart</a>
+          </div>    
+        </div>
+
+      </div>
+
 
     </div>
   )
